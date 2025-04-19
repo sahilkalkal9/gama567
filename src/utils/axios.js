a6bf7929a90a376fbd44e61531bffb2a4ab2059a
Sahil Kalkal <sahilkalkal108@gmail.com>
2025-04-19 19:57:50 +0530

    added charts

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiInstance = axios.create({
-    baseURL: "https://vip-api.kglame.com/",
+    baseURL: "https://vip-api.kglame.com",
 });
 
 export default apiInstance
